9217e2d7f330790038b3eaa5491f70e85001a818
Daniel Micay <danielmicay@gmail.com>
2017-05-16 17:51:48 -0400

    usb: add toggle for disabling newly added USB devices
    Based on the public grsecurity patches.
    
    [thibaut.sautereau@ssi.gouv.fr: Adapt to sysctl code refactoring]
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -5309,6 +5309,9 @@ static int descriptors_changed(struct usb_device *udev,
 	return changed;
 }
 
+/* sysctl */
+int deny_new_usb __read_mostly = 0;
+
 static void hub_port_connect(struct usb_hub *hub, int port1, u16 portstatus,
 		u16 portchange)
 {
@@ -5370,6 +5373,12 @@ static void hub_port_connect(struct usb_hub *hub, int port1, u16 portstatus,
 			goto done;
 		return;
 	}
+
+	if (deny_new_usb) {
+		dev_err(&port_dev->dev, "denied insert of USB device on port %d\n", port1);
+		goto done;
+	}
+
 	if (hub_is_superspeed(hub->hdev))
 		unit_load = 150;
 	else
